85c7e0899677f1ecdfbb6cbf0b725caab5c706bd
abhishekhojha <abhishekhojha45@gmail.com>
2025-05-23 18:56:47 +0530

    fix: correct typo in route handler definition

--- Api/index.js
+++ Api/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-aap.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/auth", authRoutes);
